8f446985bf64198ebaaca8567458a2da50a9b422
Eduardo Weber <edu.weber07@gmail.com>
2021-07-19 09:46:28 -0300

    Inserção de Solução

---
+++ TrabalhoEmDupla2CadastroDePessoas/TrabalhoEmDupla2CadastroDePessoas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoEmDupla2CadastroDePessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
